5de14682033d99c5c2a929e6b6dd1ee4b541e74c
can <148437203+Can-31@users.noreply.github.com>
2026-04-28 23:04:47 +0200

    scalanie

---
+++ sortowanie przez scalanie.py
@@ -0,0 +1,10 @@
+def merge_sort(t):
+    if len(t) <= 1:
+        return t
+    m = len(t) // 2
+    l = merge_sort(t[:m])
+    r = merge_sort(t[m:])
+    return sorted(l + r)
+
+
+print(merge_sort([5, 2, 9, 1]))
